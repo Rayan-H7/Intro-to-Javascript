4a595261eb46ad3e34cefdb233e71060c726b448
Rayan Hassan <98430041+Rayan-H7@users.noreply.github.com>
2024-07-15 13:31:49 +0100

    External JS files

--- main.js
+++ main.js
@@ -8,3 +8,10 @@ document.getElementById("demo").style.display = "none";
 document.getElementById("demo").style.display = "block";
 //Shows hidden elements of HTML
 //You put Javascript code in <script>
+//Functions are blocks of code that can be called when an event is called
+//Javascript functions can be placed in <head> and <body>  
+//<script>'s are put inbetween <head> and <body>
+//Scripts can be externally with the file, URL and file path 
+//<script src="myScript.js"></script>
+//<script src="https://www.w3schools.com/js/myScript.js"></script>
+//<script src="/js/myScript.js"></script>
